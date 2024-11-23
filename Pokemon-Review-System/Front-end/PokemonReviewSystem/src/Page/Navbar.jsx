f111993f2fb61aafdf6d3044233eff28ff539c34
vukt2004 <tatcatronghuvo@gmail.com>
2024-11-23 12:13:23 +0700

    Modify The Navbar of Pokemon-Review-System

--- Pokemon-Review-System/Front-end/PokemonReviewSystem/src/Page/Navbar.jsx
+++ Pokemon-Review-System/Front-end/PokemonReviewSystem/src/Page/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Layout, Menu } from 'antd';
-import './css/Navbar.css'; // Importing the CSS file
+import './css/Navbar.css';
 
 const { Header } = Layout;
 
@@ -7,13 +7,27 @@ function Navbar() {
     return (
         <Layout>
             <Header className="navbar">
-                <div className="navbar-brand">Pokémon Review</div>
-                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-                    <Menu.Item key="1"><a href="/">Home</a></Menu.Item>
-                    <Menu.Item key="2"><a href="/reviews">Reviews</a></Menu.Item>
-                    <Menu.Item key="3"><a href="/about">About</a></Menu.Item>
-                    <Menu.Item key="4"><a href="/contact">Contact</a></Menu.Item>
-                </Menu>
+                <div className="navbar-content">
+                    <div className="navbar-brand">
+                        <img 
+                            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" 
+                            alt="Pokemon"
+                            className="navbar-logo"
+                        />
+                        <span>Pokémon Review</span>
+                    </div>
+                    <Menu 
+                        theme="dark" 
+                        mode="horizontal" 
+                        className="nav-menu"
+                        defaultSelectedKeys={['1']}
+                    >
+                        <Menu.Item key="1"><a href="/">Home</a></Menu.Item>
+                        <Menu.Item key="2"><a href="/reviews">Reviews</a></Menu.Item>
+                        <Menu.Item key="3"><a href="/about">About</a></Menu.Item>
+                        <Menu.Item key="4"><a href="/contact">Contact</a></Menu.Item>
+                    </Menu>
+                </div>
             </Header>
         </Layout>
     );
